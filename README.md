7534aa648415b4c431057dc9f9a2c9360cf2e023
Zubov Illia <contactduodiamonds@gmail.com>
2022-07-11 13:01:27 +0300

    initialization project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TensionX-test_task
+
+[Demo-Link](https://zubov-illia.github.io/tensionx-test_task/)
+
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>I am Title</h1>
+    </div>
+  );
+}
+
+export default App;
